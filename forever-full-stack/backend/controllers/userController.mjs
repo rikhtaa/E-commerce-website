096ffbecd57a08d54b6db9de5ed464f112fdc1c9
Rekhta Menahil <rekhta674@gmail.com>
2025-09-06 14:49:56 +0500

    Update and rename userController.js to userController.mjs

--- forever-full-stack/backend/controllers/userController.mjs
+++ forever-full-stack/backend/controllers/userController.mjs
@@ -1,7 +1,7 @@
 import validator from "validator";
 import bcrypt from "bcrypt"
 import jwt from 'jsonwebtoken'
-import userModel from "../models/userModel.js";
+import userModel from "../models/userModel.mjs";
 
 
 const createToken = (id) => {
