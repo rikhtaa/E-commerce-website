47131662145d6a552923d4a43f3a676f26195831
Rekhta Menahil <rekhta674@gmail.com>
2025-09-06 14:55:38 +0500

    Update server.mjs

--- forever-full-stack/backend/server.mjs
+++ forever-full-stack/backend/server.mjs
@@ -1,12 +1,12 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import connectDB from './config/mongodb.js'
-import connectCloudinary from './config/cloudinary.js'
-import userRouter from './routes/userRoute.js'
-import productRouter from './routes/productRoute.js'
-import cartRouter from './routes/cartRoute.js'
-import orderRouter from './routes/orderRoute.js'
+import connectDB from './config/mongodb.mjs'
+import connectCloudinary from './config/cloudinary.mjs'
+import userRouter from './routes/userRoute.mjs'
+import productRouter from './routes/productRoute.mjs'
+import cartRouter from './routes/cartRoute.mjs'
+import orderRouter from './routes/orderRoute.mjs'
 
 // App Config
 const app = express()
